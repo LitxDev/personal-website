619983b57ab9b7ef4bcf4163ac8b189f6ee6526d
LitxDev <litxdev@gmail.com>
2021-10-30 10:02:42 +0200

    updated seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,42 @@
 export default {
+  defaultTitle: "litxdev",
+  description: `LitxDev's personal website`,
+  additionalMetaTags: [
+    {
+      property: "keywords",
+      content: "readme, easiest, create, markdown, git, github, gitlab",
+    },
+    {
+      name: "theme_color",
+      content: "#3B82F6",
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
   openGraph: {
     type: "website",
-    locale: "en_IE",
-    url: "https://litxdev.vercel.app/",
+    locale: "en_US",
+    url: "https://litxdev.vercel.app",
+    title: "readme.so",
+    description: `LitxDev's personal website`,
+    images: [
+      {
+        url: "https://litxdev.vercel.app/assets/logo.svg",
+        width: 375,
+        height: 375,
+        alt: "Logo",
+      },
+    ],
     site_name: "litxdev",
+    imageWidth: 1200,
+    imageHeight: 1200,
+  },
+  twitter: {
+    handle: "@LitxDev",
+    cardType: "summary",
   },
 }
